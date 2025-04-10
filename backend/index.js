8b623e7bce6f4388b6d3b5c9fb02f7e96dd093b1
Yash Arya <yasharya82003@gmail.com>
2025-04-10 22:19:50 +0530

    Database connected

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+dotenv.config({
+    path:'./.env'
+})
+
+
+const app=express();
+app.use(cookieParser())
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+app.use(express.json()) 
+app.use(express.urlencoded({extended:false}))
+app.use(express.static("public"))
+
+// ROUTES
+
+// MONGOOSE SETUP
+
+//Connect database function
+const connectDB=async()=>{
+    try{
+        const connectionInstance=await mongoose.connect(process.env.MONGO_URL)
+        console.log(`MongoDB connected!, DB HOST : ${connectionInstance.connection.host}`)
+    }catch(error){
+        console.log("MONGODB connection error",error);
+        process.exit(1);
+    }
+}
+
+
+
+// starting server
+const PORT=process.env.PORT || 8000
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>console.log(`Server has started on Port: ${PORT}`))
+
+
+}).catch((error)=>console.log(`${error} did not connect`))
